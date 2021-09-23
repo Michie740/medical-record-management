bc7247d58022efb71d72211d181f8458328427c5
Simran Kaur Soin <simranksoin@gmail.com>
2021-09-23 14:58:08 -0400

    Add home page with link to sign up view

--- medical_record_management/users/urls.py
+++ medical_record_management/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.UserCreateView.as_view(), name='sign_up'),
+    path('', TemplateView.as_view(template_name="users/home.html"), name='home'),
+    path('sign_up/', views.UserCreateView.as_view(), name='sign_up'),
 ]
